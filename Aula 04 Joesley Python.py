0889a84b94dd1f6384c4b3e70ddb1fa68be7aeec
joesleysenai <joesley.santana@ba.estudante.senai.br>
2026-09-12 16:18:37 -0300

    Aula 04 Joesley
    Professores aparentemente desistiram de dar liberdade aos alunos para escolherem a linguagem após extrema variação. Foi decidido que as atividades devem ser feitas em Python. Essa é a versão da aplicação já transformada em Python. A prática foi um bom exercício para relembrar como a linguagem funciona e como é prático o uso da indentação para recuo do else da função buscar_usuario.

---
+++ Aula 04 Joesley Python.py
@@ -0,0 +1,87 @@
+def exibir_lista(cadastrados, nomes, idades):
+    if cadastrados == 0:
+        print("Nenhum usuario cadastrado ainda.")
+        return 
+
+    print("\n========== Usuarios cadastrados ==========")
+
+    for i in range(cadastrados):
+        print(f"Nome: {nomes[i]}")
+        print(f"Idade: {idades[i]}")
+        print("-------------------------")
+
+def buscar_usuario(cadastrados, nomes, idades):
+    if cadastrados == 0:
+        print("Nenhum usuario cadastrado ainda.")
+        return
+    print("\n========== Buscar Usuario ==========")
+    busca = (input("\nDigite o nome do usuario :"))
+    for i in range(cadastrados):
+        if busca == nomes[i]:
+            print(f"Nome: {nomes[i]}")
+            print(f"Idade: {idades[i]}")
+            print(f"Posição: {i}")
+            print("-------------------------")
+            break
+    else:
+        print("Usuario nao encontrado!")
+            
+
+def exibir_menu():
+    print("\n================= Menu Principal ==================")
+    print("1 - Cadastrar novo usuario")
+    print("2 - Listar todos os usuarios cadastrados")
+    print("3 - Buscar usuario")
+    print("4 - Sair do sistema")
+
+
+def main():
+    cadastrados = 0
+
+    limite = int(input("Quantos usuarios serao cadastrados no maximo? "))
+
+    if limite <= 0:
+        print("O limite deve ser maior que zero.")
+        return
+
+    nomes = []
+    idades = []
+
+    opcao = 0
+
+    while opcao != 4:
+        exibir_menu()
+        opcao = int(input("Opcao: "))
+
+        if opcao == 1:
+            if cadastrados < limite:
+                nome = input("Digite o nome do usuario: ")
+                idade = int(input("Digite a idade do usuario: "))
+
+                nomes.append(nome)
+                idades.append(idade)
+
+                cadastrados += 1
+
+                print(
+                    f"Usuario cadastrado com sucesso! "
+                    f"({cadastrados}/{limite})"
+                )
+            else:
+                print("Limite maximo de cadastros atingido!")
+
+        elif opcao == 2:
+            exibir_lista(cadastrados, nomes, idades)
+
+        elif opcao == 3:
+            buscar_usuario(cadastrados, nomes, idades)
+
+        elif opcao == 4:
+            print("Encerrando o sistema...")
+            return
+        else:
+            print("Opcao invalida! Tente novamente.")
+
+
+if __name__ == "__main__":
+    main()
